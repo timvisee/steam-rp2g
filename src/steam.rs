5f75ab54e07aae3e5255047953c0a80de4d90117
timvisee <tim@visee.me>
2020-10-10 02:16:21 +0200

    Improve game binary filter, determine based on whitelist suffixes

--- src/steam.rs
+++ src/steam.rs
@@ -47,6 +47,46 @@ pub fn find_game_bins(dir: &Path) -> Vec<PathBuf> {
     crate::fs::ls(dir)
         .expect("failed to list Steam game dirs")
         .into_iter()
-        .filter(|f| f.is_file())
+        .filter(|f| is_bin(&f))
         .collect()
+}
+
+/// Check whether given file is considered a binary.
+pub fn is_bin(path: &Path) -> bool {
+    // Must be a file
+    if !path.is_file() {
+        return false;
+    }
+
+    // Get file and parent directory name
+    let name = match path
+        .file_name()
+        .and_then(|n| n.to_str())
+        .map(|n| n.to_string())
+    {
+        Some(n) => n.to_lowercase(),
+        None => return false,
+    };
+    let parent = match path.parent() {
+        Some(p) => p,
+        None => return false,
+    };
+    let parent_name = match parent
+        .file_name()
+        .and_then(|n| n.to_str())
+        .map(|n| n.to_string())
+    {
+        Some(n) => n.to_lowercase(),
+        None => return false,
+    };
+
+    // TODO: possibly check if file is executable?
+
+    // Whitelist of parent directory names and binary suffixes
+    let parents = ["bin", "binary", "run"];
+    let suffixes = [".exe", ".x86", ".x86_64", ".bin", ".linux", "64"];
+
+    parent_name == name || parents.iter().any(|n| &parent_name == n) || {
+        suffixes.iter().any(|e| name.ends_with(e))
+    }
 }
